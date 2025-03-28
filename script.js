dc2c27c1bd9d6574eb3b72c7b511a2cc85674b37
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-03-28 19:14:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+	const form = document.getElementById("userForm");
+	const inputs = form.elements;
+
+	for(let input of inputs){
+		if(input.name && localStorage.getItem(input.name)){
+			input.value = localStorage.getItem(input.name);
+		}
+	}
+
+	form.addEventListener("input", function (event) {
+  if(event.target.name){
+	  localStorage.setItem(event.target.name, event.target.value);
+  }		
+	});
+
+	document.getElementById("clear").addEventListener("click", function () {
+		localStorage.clear();
+		form.reset();
+		
+	})
+	
+})
